7cbd4fab284663536cf09fdc8c93da75c909e769
Noah Wimmer <nwimmer123@yahoo.com>
2016-07-26 14:31:39 -0700

    added routes to server

--- server.js
+++ server.js
@@ -1,16 +1,34 @@
 var express = require('express');
 var app = express();
-var html = require('html');
+
+app.set('view engine', 'ejs');
 
 app.use('/',express.static(__dirname + '/public'));
 
 // ROUTES
 
-app.get('/', function homepage (req, res) {
-  res.sendFile(__dirname + '/index.html');
+app.get('/', function(req,res) {
+  res.render('pages/index');
 });
 
-// SERVER 
+app.get('/about_me', function(req,res) {
+  res.render('pages/about_me');
+});
+
+app.get('/contact', function(req,res) {
+  res.render('pages/contact');
+});
+
+app.get('/portfolio', function(req,res) {
+  res.render('pages/portfolio');
+});
+
+
+app.get('/error', function(req,res) {
+  res.render('pages/error');
+});
+
+//SERVER
 
 app.listen(3000);
-console.log("I'm up!!")
+console.log('TITTIES!!!');
