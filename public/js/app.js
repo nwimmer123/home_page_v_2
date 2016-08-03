3956d4edb8816290a4878aa96587f75662408b08
Noah Wimmer <nwimmer123@yahoo.com>
2016-08-02 17:17:52 -0700

    added tester portfolio pics

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
 
+  $('#andersen_text').hide();
+
+  $('#andersen_pic').hover(function() {
+    $('#andersen_text').show();
+    $('#andersen_pic').hide();
+  });
+
+  $('#andersen_pic').mouseover(function() {
+    console.log("CLICKED");
+    $('#andersen_text').show();
+    $('#andersen_pic').hide();
+  });
+
 });
