afa42153b1c531acf6170d433f1d5588b38ffad5
Noah Wimmer <nwimmer123@yahoo.com>
2016-08-03 15:53:03 -0700

    added jquery mobile and tap function for mobile

--- public/js/app.js
+++ public/js/app.js
@@ -14,4 +14,16 @@ $(document).ready(function () {
     $('#test_text').hide();
   });
 
+  $('#test_pic').tap(function () {
+    $(this).stop().animate({
+        opacity: .1
+    }, 200);
+    $('#test_text').show();
+  }, function () {
+    $(this).stop().animate({
+        opacity: 1
+    }, 500);
+    $('#test_text').hide();
+  });
+
 });
