22e9f02344275f1ea3c58a499e7f29ee1b3ea014
Noah Wimmer <nwimmer123@yahoo.com>
2016-08-03 17:16:01 -0700

    fleshed out server basics

--- server.js
+++ server.js
@@ -9,3 +9,29 @@ var html = require('html');
 
 var configDB = require('./config/database.js');
 
+app.use('/',express.static(__dirname + '/public'));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/************
+ * DATABASE *
+ ************/
+
+var db = require('./models');
+
+/**********
+ * ROUTES *
+ **********/
+
+app.get('/', function homepage (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+/**********
+ * SERVER *
+ **********/
+
+// listen on port 3000
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Express server is running on http://localhost:3000/');
+});
