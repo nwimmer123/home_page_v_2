bc9448911b32527438bb68dc548fa11302d7c5f6
Noah Wimmer <nwimmer123@yahoo.com>
2016-08-03 20:53:13 -0700

    made data schema for portfolio items

--- models/portfolio_item.js
+++ models/portfolio_item.js
@@ -1,1 +1,17 @@
-portfolio_items.js
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var Portfolio_ItemSchema = new Schema({
+  description: String,
+  language: String,
+  framework: String,
+  library: String,
+  database: String,
+  other: String,
+  sitelink: String,
+  githublink: String
+});
+
+var Portfolio_Item = mongoose.model('Portfolio_Item', Portfolio_ItemSchema);
+
+module.exports = Portfolio_Item;
