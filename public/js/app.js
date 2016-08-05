ba6f443c9f0c85cdb2fc5d197ec6fa051e601522
Noah Wimmer <nwimmer123@yahoo.com>
2016-08-04 17:18:14 -0700

    added handlebars template and comments

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,14 @@
 $(document).ready(function () {
 
+  var baseUrl = '/api/portfolio_items';
+
+  //handlebars set up
+
+  var source = $('#portfolio_items-template').html();
+  var template = Handlebars.compile(source);
+
+  //reveal info about portfolio item
+
   $('#test_text').hide();
 
   $('#test_pic').hover(function () {
@@ -13,6 +22,8 @@ $(document).ready(function () {
     }, 500);
     $('#test_text').hide();
   });
+
+  //reveal info about portfolio item for mobile
 
   $('#test_pic').tap(function () {
     $(this).stop().animate({
