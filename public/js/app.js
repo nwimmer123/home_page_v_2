cdcae8fe6873b3178218f65e5ca481c8e9019327
Noah Wimmer <nwimmer123@yahoo.com>
2016-08-05 22:10:25 -0700

    fading images succesful

--- public/js/app.js
+++ public/js/app.js
@@ -14,61 +14,6 @@ $(document).ready(function () {
     $("#portfolio_items-list").append(dataHtml);
   });
 
-  //reveal info about portfolio item
-
-  // $('.test_text').hide();
-
-  // $('.test_pic').hover(function () {
-  //   $(this).stop().animate({
-  //       opacity: .1
-  //   }, 200);
-  //   $('.test_text').show();
-  // }, function () {
-  //   $(this).stop().animate({
-  //       opacity: 1
-  //   }, 500);
-  //   $('.test_text').hide();
-  // });
-
-  //experimental
-
-  // var id = 0;
-  
-
-  // while (id < 3) {
-  //   $('#text' + id).hide();
-
-  //   $('#pic' + id).hover(function () {
-  //     $(this).stop().animate({
-  //         opacity: .1
-  //     }, 200);
-  //     $(textid).show();
-  //   }, function () {
-  //     $(this).stop().animate({
-  //         opacity: 1
-  //     }, 500);
-  //     $('#text' +  id).hide();
-  //   });
-  //   id ++;
-  
-
-  // };
-
-  //reveal info about portfolio item for mobile
-
-  // $('#test_pic').tap(function () {
-  //   $(this).stop().animate({
-  //       opacity: .1
-  //   }, 200);
-  //   $('#test_text').show();
-  // }, function () {
-  //   $(this).stop().animate({
-  //       opacity: 1
-  //   }, 500);
-  //   $('#test_text').hide();
-  // });
-
-
 });
 
 
